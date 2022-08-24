073aa171433b2e14ffe92f5a966ab1bb17f56df6
barrettotte <barrettotte@gmail.com>
2022-08-23 22:31:34 -0400

    starting interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        2 => run_file(&args[1]),
+        _ => println!("Usage: cargo run [file_path]")
+    }
+}
+
+fn run_file(path: &str) {
+    println!("Running {}", path);
+    // lexer new
+    // parser new
+
+    if let Ok(f) = File::open(path) {
+        // lex file to tokens
+        // parse tokens to AST
+        // beta reduce AST
+        // return AST
+    } else {
+        println!("Could not find file {}", path);
+    }
 }
---
+++ src/token.rs
@@ -0,0 +1,7 @@
+#[derive(Debug)]
+pub enum Token {
+    Lambda,
+    Dot,
+    Variable,
+    EOF,
+}
